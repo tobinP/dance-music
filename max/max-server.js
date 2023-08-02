42487ae3a5e9d0d75567e96deb0ff71baf89f349
tobin <tobin.pomeroy@gmail.com>
2023-08-01 21:05:02 -0700

    Update max graphs

--- max/max-server.js
+++ max/max-server.js
@@ -1,27 +1,35 @@
 const maxAPI = require("max-api");
 let repeater
 let isRunning = false
+let noteOn = 100
+let noteOff = 0
+let noteNoChange = -1
 maxAPI.outlet("hey from server")
-maxAPI.addHandler("text", (...args) => {
+maxAPI.addHandler("server", (...args) => {
 	maxAPI.outlet(args[0])
 	if (args[0] === "autorun") {
 		maxAPI.outlet("autorun toggled")
 		if (isRunning) {
-			clearInterval(repeater)
+			// clearInterval(repeater)
+			maxAPI.outlet([noteOff])
 		} else {
-			autoRun(-1, 5, 40, 2)
+			// autoRun(noteNoChange, 5, 40, 2)
+			maxAPI.outlet([noteOn])
 		}
 		isRunning = !isRunning
 	}
 });
 
+
+
 const WebSocket = require("ws")
 const wss = new WebSocket.Server({ port: 9090 });
 wss.on('connection', function connection(ws) {
 	ws.on('message', function message(data) {
-		maxAPI.outlet("message received by server")
+		// maxAPI.outlet("message received by server")
 		let decoded = JSON.parse(data)
-		maxAPI.outlet([decoded.name, decoded.xVal])
+		let val = map(decoded.xVal)
+		maxAPI.outlet([decoded.name, val])
 	});
 	let obj = {
 		name: -1,
@@ -29,9 +37,29 @@ wss.on('connection', function connection(ws) {
 	}
 	ws.send(JSON.stringify(obj));
 
-	autoRun(-1, 5, 40, 2)
+	// autoRun(noteNoChange, 5, 40, 2)
 
 });
+
+let min1 = 1
+let max1 = 2
+let min2 = 25
+let max2 = 100
+
+let diff1 = max1 - min1
+let diff2 = max2 - min2
+let ratio = diff2 / diff1
+
+function map(val) {
+	// if (val < 1) val = 1
+	// if (val > 2) val = 2
+	// return val * ratio
+
+	val = val * 100
+	if (val > 126) val - 126
+	return val
+}
+
 function autoRun(event, min, max, incrementValue) {
 	let value = 0
 	let shouldIncrease = true
@@ -48,10 +76,6 @@ function autoRun(event, min, max, incrementValue) {
 				shouldIncrease = true
 			}
 		}
-		let obj = {
-			name: event,
-			xVal: value
-		}
-		maxAPI.outlet([obj.name, obj.xVal])
+		maxAPI.outlet([event, value])
 	}, 75)
 }
